57edc9e63c9086b2346414f2a0c0fd3f174174d1
rahimnsubuga <rahimnsubuga654@gmail.com>
2025-03-22 10:39:09 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,76 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    char n[40];
+     printf("Please enter domestic animals:");
+     scanf("%s",&n);
 
-void Physics(){
+     char m[30];
+     printf("Please enter wild animals:");
+     scanf("%s",&m);
 
-int num_a,num_b,num_c,num_d;
+     char k[70]="n+m";
+     printf("The group of n and m is %s,%s",n,m);
 
-printf("Please enter the marks for Beginning of term:",num_a);
-scanf("%d",&num_a);
-
-printf("Please enter the marks for Midterm:",num_b);
-scanf("%d",&num_b);
-
-printf("Please enter the marks for coursework:",num_c);
-scanf("%d",&num_c);
-
-printf("Please enter the marks for End of term:",num_d);
-scanf("%d",&num_d);
-
-int s=num_a+num_b+num_c+num_d;
-
- float average=s/4.0;
- printf("The average of physics:%.2f\n",average);
-
-}
-
-void Chemistry(){
-int num_a,num_b,num_c,num_d;
-
-printf("Please enter the marks for Beginning of term:",num_a);
-scanf("%d",&num_a);
-
-printf("Please enter the marks for Midterm:",num_b);
-scanf("%d",&num_b);
-
-printf("Please enter the marks for coursework:",num_c);
-scanf("%d",&num_c);
-
-printf("Please enter the marks for End of term:",num_d);
-scanf("%d",&num_d);
-
-int s=num_a+num_b+num_c+num_d;
-
- float average=s/4.0;
- printf("The average of chemistry:%.2f\n",average);
-
-}
-
-void Maths(){
-int num_a,num_b,num_c,num_d;
-
-printf("Please enter the marks for Beginning of term:",num_a);
-scanf("%d",&num_a);
-
-printf("Please enter the marks for Midterm:",num_b);
-scanf("%d",&num_b);
-
-printf("Please enter the marks for coursework:",num_c);
-scanf("%d",&num_c);
-
-printf("Please enter the marks for End of term:",num_d);
-scanf("%d",&num_d);
-
-int s=num_a+num_b+num_c+num_d;
-
- float average=s/4.0;
- printf("The average of maths:%.2f\n",average);
-
-}
-
-int main(){
-Physics();
-Chemistry();
-Maths();
-
-return 0;
+    return 0;
 }
